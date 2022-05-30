328c92bf6316c038d800255af716799d7b8bad77
Knob <59198478+nbr41to@users.noreply.github.com>
2022-05-30 11:46:32 +0900

    .

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const container = document.getElementById('container');
+    if (container) {
+      container.focus();
+    }
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
